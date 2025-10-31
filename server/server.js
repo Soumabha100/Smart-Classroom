835cc3ef393bcce24cddf0766b9c1afa2b68bbb5
Soumabha Majumder <soumabhamajumder@gmail.com>
2025-11-01 00:56:22 +0530

    Server Optimized

--- server/server.js
+++ server/server.js
@@ -40,7 +40,21 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Make `io` accessible to your routes by attaching it to the request object
