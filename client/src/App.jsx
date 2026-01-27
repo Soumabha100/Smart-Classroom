dfadf21ffc640ec83d8e599e9818383250b73d2a
Soumabha100 <soumabhamajumder@gmail.com>
2026-01-27 14:51:02 +0530

    Minor Error Fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Routes, Route } from "react-router-dom";
 import { useTheme } from "./context/ThemeContext.jsx";
 import { Toaster } from "react-hot-toast";
